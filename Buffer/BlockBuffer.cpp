2055972cf3a719ce8ca5bed3dea745c8e13e7295
KevivJaknap <vivekpankaj1@gmail.com>
2024-01-10 16:48:56 +0000

    second commit

--- Buffer/BlockBuffer.cpp
+++ Buffer/BlockBuffer.cpp
@@ -10,15 +10,17 @@ BlockBuffer::BlockBuffer(int blockNum) {
 RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
 
 int BlockBuffer::getHeader(struct HeadInfo *head){
-    unsigned char buffer[BLOCK_SIZE];
-
-    Disk::readBlock(buffer, blockNum);
-
-    memcpy(&head->numSlots, buffer+24, 4);
-    memcpy(&head->numEntries, buffer+16, 4);
-    memcpy(&head->numAttrs, buffer+20, 4);
-    memcpy(&head->rblock, buffer+12, 4);
-    memcpy(&head->lblock, buffer+8, 4);
+    unsigned char *bufferPtr;
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+    if (ret != SUCCESS) {
+        return ret;
+    }
+       
+    memcpy(&head->numSlots, bufferPtr+24, 4);
+    memcpy(&head->numEntries, bufferPtr+16, 4);
+    memcpy(&head->numAttrs, bufferPtr+20, 4);
+    memcpy(&head->rblock, bufferPtr+12, 4);
+    memcpy(&head->lblock, bufferPtr+8, 4);
 
     return SUCCESS;
 }
